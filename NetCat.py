4656e766e15af6c09114ca97ae2acc784902705f
Rootzir0 <rootzir000@gmail.com>
2024-06-18 22:21:49 -0400

    completed

--- NetCat.py
+++ NetCat.py
@@ -27,13 +27,13 @@ if __name__ == '__main__':
             echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
             netcat.py -t 192.168.1.108 -p 5555 # connect to server
         '''))
-    parser.add_argument('c','--command',action='store_true',help='command shell')
+    parser.add_argument('-c','--command',action='store_true',help='command shell')
     parser.add_argument('-e','--execute',help='execute specified command')
     parser.add_argument('-l','--listen',action='store_true',help='listen')
     parser.add_argument('-p','--port',type=int,default=5555,help='specified port')
     parser.add_argument('-t','--target',default='192.168.1.203',help='target ip')
     parser.add_argument('-u','--upload',help='upload file')
-    args = parser.pars_args()
+    args = parser.parse_args()
     if args.listen:
         buffer = ''
     else:
@@ -101,8 +101,31 @@ def handle(self,client_socket):
         client_socket.send(output.encode())
 
     elif self.args.upload:
-        file_buffer = b"
-        
-        
-        
-        "
+        file_buffer = b""
+        while True:
+            data = client_socket.recv(4096)
+            if data:
+                file_buffer += data
+            else:
+                break
+
+            with open(self.args.upload, 'wb')as f:
+                f.write(file_buffer)
+            message = f'saved file{self.args.upload}'
+            client_socket.send(message.encode())
+
+    elif self.args.command:
+        cmd_buffer = "b"
+        while True:
+            try:
+                client_socket.send(b'BHP:#>')
+                while '\n' not in cmd_buffer.decode():
+                    cmd_buffer += client_socket.recv(64)
+                response = execute(cmd_buffer.decode())
+                if response:
+                    client_socket.send(response.encode())
+                cmd_buffer = b""
+            except Exception as e:
+                print(f'server killed{e}')
+                self.socket.close()
+                sys.exit()
