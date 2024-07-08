83c98cfd0191bd0c17a71ede4fff42bb54f276b9
Rootzir0 <rootzir000@gmail.com>
2024-07-08 04:45:34 -0400

    comments added

--- tcp-proxy.py
+++ tcp-proxy.py
@@ -2,9 +2,12 @@ import sys
 import socket
 import threading
 
+from inflect import Word
+
 HEX_FILTER = ''.join(
     [(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
 
+#Display communication between hosts
 def hexdump(src,length=16,show=True):
     if isinstance(src,bytes):
         src=src.decode()
@@ -12,3 +15,7 @@ def hexdump(src,length=16,show=True):
     results = list()
     for i in range(0,len(src),length):
         word = str(src[i:i+length])
+
+printable = Word.translate(HEX_FILTER)
+hexa =''.join([f'{ord(c):02X}'for c in Word])
+hexwidth= length*3 # type: ignore      
